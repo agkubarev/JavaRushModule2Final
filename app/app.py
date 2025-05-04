b413fe652182d220fa8156756820eba1de5dc9a0
agkubarev <agkubarev@yandex.ru>
2025-05-04 09:50:43 +0300

    developed main functionallity

---
+++ app/app.py
@@ -0,0 +1,48 @@
+import os
+from http.server import HTTPServer
+from dotenv import load_dotenv
+from loguru import logger
+from DBManager import DBManager
+from ImageHostingHandler import ImageHostingHandler
+from Router import Router
+from settings import LOG_PATH, LOG_FILE
+from settings import SERVER_ADDRESS
+from AdvancedHandler import AdvancedHTTPRequestHandler
+
+
+
+def register_routes(router: Router, handler_class: AdvancedHTTPRequestHandler):
+    router.add_route('GET', '/api/images/', handler_class.get_images)
+    router.add_route('POST', '/upload/', handler_class.post_upload)
+    router.add_route('DELETE', '/api/delete/<image_id>',
+                     handler_class.delete_image)
+
+def run(server_class=HTTPServer, handler_class=ImageHostingHandler):
+    load_dotenv()
+    logger.add(os.path.join(LOG_PATH, LOG_FILE),
+               format='[{time:YYYY-MM-DD HH:mm:ss}] {level}: {message}',
+               level='INFO')
+
+    db = DBManager(os.getenv('POSTGRES_DB'),
+                   os.getenv('POSTGRES_USER'),
+                   os.getenv('POSTGRES_PASSWORD'),
+                   os.getenv('POSTGRES_HOST'),
+                   os.getenv('POSTGRES_PORT'))
+    db.init_tables()
+
+    router = Router()
+    register_routes(router, handler_class)
+
+    httpd = server_class(SERVER_ADDRESS, handler_class)
+    logger.info(f'Serving on http://{SERVER_ADDRESS[0]}:{SERVER_ADDRESS[1]}')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        logger.warning('Keyboard interrupt received, exiting.')
+        httpd.server_close()
+    finally:
+        logger.info('Server stopped.')
+
+
+if __name__ == '__main__':
+    run()
